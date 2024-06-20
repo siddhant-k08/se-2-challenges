5a189a41cdc845875b9f0d70072563f9f840b195
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-06-20 16:20:18 +0530

    Initial commit

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -3,18 +3,28 @@ pragma solidity 0.8.4;  //Do not change the solidity version as it negativly imp
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Staker {
-
+  IERC20 public immutable stakingToken;
   ExampleExternalContract public exampleExternalContract;
 
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  mapping (address => uint256) public balances;
+
+  uint256 public constant threshold = 1 ether;
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+  function stake(uint256 _amount) public payable {
+      require(_amount > 0, "amount = 0");
+      stakingToken.transferFrom(msg.sender, address(this), _amount);
+      balances[msg.sender] += _amount;
 
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
